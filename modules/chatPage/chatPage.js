72fff1ec21239dcaec5f951fa05bbb5711be4c7f
Marvel Dev <marvellousalika48@gmail.com>
2021-01-25 19:24:04 +0100

    initial commit

--- modules/chatPage/chatPage.js
+++ modules/chatPage/chatPage.js
@@ -1,7 +1,36 @@
-const chatPage = () => {
+const ChatPage = () => {
   return `
-    <div>HELLO</div>
+    <div class="chat-page">
+      <div class="chat-top-nav">
+        <img src="https://images.pexels.com/photos/4119310/pexels-photo-4119310.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500" 
+          class="image" alt="photo">
+        <div class="status-info">
+          <strong>Jack Williams</strong>
+          <p>online</p>
+        </div>
+        <button type="button" class="chat-page-dropdown">
+          <i class="material-icons">&#xe5d4;</i>
+        </button>
+      </div>
+      <div id="chatInputContent">
+        <div>
+          <div class="add-buttons">
+            <button class="smiley-icon"><i class="material-icons">&#xe7f2;</i></button>
+            <button class="clip-icon"><i class="fa fa-paperclip"></i></button>
+          </div>
+          <textarea class="chat-box" placeholder="Type a message"></textarea>
+          <div>
+            <button type="button" class="record-button"><i class="fa fa-microphone"></i></button>
+            <button type="button" class="send-button"><i class="material-icons">&#xe163;</i></button>
+          </div>
+        </div>
+        <span class="person-button-container">
+          <button type="button" class="person-one-button">Person1</button>
+          <button type="button" class="person-two-button">Person2</button>
+        </span>
+      </div>
+    </div>
   `
 }
 
-export default chatPage
+export default ChatPage
