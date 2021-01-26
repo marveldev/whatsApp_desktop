7d9cab1f392147326e3709da6ce59f60d22eedc9
Marvel Dev <marvellousalika48@gmail.com>
2021-01-26 15:58:19 +0100

    add display of default page on click

--- modules/defaultNav/events.js
+++ modules/defaultNav/events.js
@@ -1,9 +1,11 @@
+import switchCurrentPage from "../helper.js";
+
 const defaultNavEvents = () => {
   const chatPersonButtons = document.querySelectorAll('.chat-person')
   for (let index = 0; index < chatPersonButtons.length; index++) {
     const chatPersonButton = chatPersonButtons[index];
     chatPersonButton.addEventListener('click', () => {
-      console.log('ok');
+      switchCurrentPage('defaultPage')
     })
   }
 }
