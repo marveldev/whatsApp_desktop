d3f2a57b969492d5e7f2acb4c3ef11e8c784038e
Marvel Dev <marvellousalika48@gmail.com>
2021-01-26 16:32:40 +0100

    change defaultPage to chatPage as argument

--- modules/defaultNav/events.js
+++ modules/defaultNav/events.js
@@ -1,11 +1,11 @@
-import switchCurrentPage from "../helper.js";
+import switchCurrentPage from "../helper.js"
 
 const defaultNavEvents = () => {
   const chatPersonButtons = document.querySelectorAll('.chat-person')
   for (let index = 0; index < chatPersonButtons.length; index++) {
     const chatPersonButton = chatPersonButtons[index];
     chatPersonButton.addEventListener('click', () => {
-      switchCurrentPage('defaultPage')
+      switchCurrentPage('chatPage')
     })
   }
 }
