167ca42f637dbfbad0d313732206a6d82817b4be
Marvel Dev <marvellousalika48@gmail.com>
2021-01-26 15:56:34 +0100

    add return html page content

--- modules/defaultPage/defaultPage.js
+++ modules/defaultPage/defaultPage.js
@@ -1,6 +1,15 @@
 const DefaultPage = () => {
   return`
-    <h1>HELLO</h1>
+    <div class="default-page">
+      <div id="defaultPage">
+        <img src="../../assets/default_page_photo.png" alt="phone in hand">
+        <p>Keep your phone connected</p>
+        <span>WhatsApp connects to your phone to sync messages.
+          To reduce data usuage, connect your phone to Wi-Fi.
+        </span>
+      </div>
+      <p>WhatsApp is available for Windows. <span>Get it here.<span</p>
+    </div>
   `
 }
 
