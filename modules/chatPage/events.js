b3386fed96e15780c89daca0f89d57db199cd352
Marvel Dev <marvellousalika48@gmail.com>
2021-01-26 12:38:15 +0100

    initial commit

---
+++ modules/chatPage/events.js
@@ -0,0 +1,71 @@
+const addChatItemToDom = person => {
+  const chatContainer = document.querySelector('.chat-container')
+  const personContainerButton = document.querySelector('.person-button-container')
+  const recordButton = document.querySelector('.record-button')
+  const sendChatButton = document.querySelector('.send-button')
+  const chatBox = document.querySelector('.chat-box')
+  const chatBoxValue = chatBox.value.trim()
+  const itemId = 'id' + Date.parse(new Date()).toString()
+  const chatTime = new Date().toTimeString().substr(0, 5)
+
+  const chatItem = `
+    <div id="${itemId}">
+      <div class="${person === 'person-one' ? 'arrow-right' : 'arrow-left'}"></div>
+      <div class="chat-item-overlay"></div>
+      <div class="${person} chat-item">
+        <div id="${person}" class="chat-text">
+          <span class="message-value">${chatBoxValue}</span>
+          <sub class="chat-time">${chatTime}</sub>
+        </div>
+      </div>
+    </div>
+  `
+
+  chatContainer.innerHTML += chatItem
+  chatContainer.scrollTop = chatContainer.scrollHeight
+  chatBox.style.height = ''
+  chatBox.value = ''
+  recordButton.style.display = 'block'
+  sendChatButton.style.display = 'none'
+  personContainerButton.style.display = 'none'
+
+  // const chatObject = {
+  //   itemId: itemId,
+  //   person: person,
+  //   chatTime: chatTime,
+  //   chatBoxValue: chatBoxValue
+  // }
+
+  // addEntryToDb('chatData', chatObject)
+  // chatItemEvents()
+}
+
+const chatPageEvents = () => {
+  const chatBox = document.querySelector('.chat-box')
+  const sendChatButton = document.querySelector('.send-button')
+  const recordButton = document.querySelector('.record-button')
+  const personOneChatButton = document.querySelector('.person-one-button')
+  const personTwoChatButton = document.querySelector('.person-two-button')
+
+  chatBox.addEventListener('input', () => {
+    chatBox.style.height = "1px"
+    chatBox.style.height = (3+chatBox.scrollHeight)+"px"
+    if (chatBox.value.trim().length >= 1) {
+      sendChatButton.style.display = 'block'
+      recordButton.style.display = 'none'
+    } else {
+      sendChatButton.style.display = 'none'
+      recordButton.style.display = 'block'
+      document.querySelector('.person-button-container').style.display = 'none'
+    }
+  })
+
+  sendChatButton.addEventListener('click', () => {
+    document.querySelector('.person-button-container').style.display = 'block'
+  })
+
+  personOneChatButton.addEventListener('click', () => addChatItemToDom('person-one'))
+  personTwoChatButton.addEventListener('click', () => addChatItemToDom('person-two'))
+}
+
+export default chatPageEvents
