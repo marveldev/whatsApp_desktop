dc3764a6e9a54c7197ce6be7346641907a9a42a5
Marvel Dev <marvellousalika48@gmail.com>
2021-01-27 10:06:01 +0100

    add chatItemEvent import

--- modules/helper.js
+++ modules/helper.js
@@ -1,10 +1,10 @@
 import DefaultPage from "./defaultPage/defaultPage.js"
 import ChatPage from "./chatPage/chatPage.js"
-import chatPageEvents from "./chatPage/events.js"
+import chatPageEvents, { chatItemEvents } from "./chatPage/events.js"
 import DefaultNav from "./defaultNav/defaultNav.js"
 import defaultNavEvents from "./defaultNav/events.js"
 
-const switchCurrentPage = (page) => {
+const switchCurrentPage = async (page) => {
   const currentPage = document.querySelector('.current-page')
   const currentNav = document.querySelector('.current-nav')
   localStorage.setItem('currentPage', page)
@@ -15,8 +15,9 @@ const switchCurrentPage = (page) => {
       defaultNavEvents()
       break;
     case 'chatPage':
-      currentPage.innerHTML = ChatPage()
+      currentPage.innerHTML = await ChatPage()
       chatPageEvents()
+      chatItemEvents()
       break;
     default:
       currentPage.innerHTML = DefaultPage()
