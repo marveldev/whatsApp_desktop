4d58f4c83dce8e884694a415727c6b7af47f6a54
Marvel Dev <marvellousalika48@gmail.com>
2021-01-27 18:26:53 +0100

    add page-overlay div

--- App.js
+++ App.js
@@ -1,11 +1,11 @@
 import { request } from "./dataStorage.js"
 import DefaultNav from "./modules/defaultNav/defaultNav.js"
 import defaultNavEvents from "./modules/defaultNav/events.js"
-import DefaultPage from "./modules/defaultPage/defaultPage.js"
 import switchCurrentPage from "./modules/helper.js"
 
 const App = () => {
   return `
+    <section class="page-overlay"></section>
     <section class="page-container">
       <div class="current-nav">
         ${DefaultNav()}
