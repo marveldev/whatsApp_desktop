68f448f9035bcde94b881ae3a617b91aa8742f02
Marvel Dev <marvellousalika48@gmail.com>
2021-01-28 15:05:35 +0100

    add nav-dropdown-modal container

--- modules/defaultNav/DefaultNav.js
+++ modules/defaultNav/DefaultNav.js
@@ -3,10 +3,19 @@ const DefaultNav = () => {
     <div class="default-nav">
       <div class="top-nav">
         <div class="top-nav-buttons">
-          <img src="https://images.pexels.com/photos/4119310/pexels-photo-4119310.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500" class="profile-photo image" alt="">
+          <img src="https://images.pexels.com/photos/4119310/pexels-photo-4119310.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500" class="profile-photo" alt="">
           <button><i class="material-icons">&#xe1af;</i></button>
           <button class=""><i class="material-icons">&#xe0b7;</i></button>
           <button class="top-nav-dropdown"><i class="material-icons">&#xe5d4;</i></button>
+        </div>
+        <div class="nav-dropdown-modal">
+          <button>New group</button>
+          <button>Create a room</button>
+          <button>Profile</button>
+          <button>Archived</button>
+          <button>Starred</button>
+          <button>Settings</button>
+          <button>Log out</button>
         </div>
         <div id="searchBoxContainer">
           <textarea class="search-box" placeholder="Search or start a new chat"></textarea>
