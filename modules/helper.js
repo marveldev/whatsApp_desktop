ae7bc814fea4b12fc7afa30a9030a96008f875ce
Marvel Dev <marvellousalika48@gmail.com>
2021-01-28 16:50:17 +0100

    add settingsNav import

--- modules/helper.js
+++ modules/helper.js
@@ -3,16 +3,21 @@ import ChatPage from "./chatPage/ChatPage.js"
 import chatPageEvents, { chatItemEvents } from "./chatPage/events.js"
 import DefaultNav from "./defaultNav/DefaultNav.js"
 import defaultNavEvents from "./defaultNav/events.js"
+import SettingsNav from "./settingsNav/settingsNav.js"
+import settingsNavEvents from "./settingsNav/events.js"
 
 const switchCurrentPage = async (page) => {
   const currentPage = document.querySelector('.current-page')
   const currentNav = document.querySelector('.current-nav')
-  localStorage.setItem('currentPage', page)
 
   switch (page) {
     case 'defaultNav':
       currentNav.innerHTML = DefaultNav()
       defaultNavEvents()
+      break;
+    case 'settingsNav':
+      currentNav.innerHTML = SettingsNav()
+      settingsNavEvents()
       break;
     case 'chatPage':
       currentPage.innerHTML = await ChatPage()
