cb2fc1fe09183b700e6ee8992bf9b1464bbc5365
Marvel Dev <marvellousalika48@gmail.com>
2021-01-29 18:47:00 +0100

    add sendTextButton event

--- modules/statusTextEntry/events.js
+++ modules/statusTextEntry/events.js
@@ -1,8 +1,10 @@
-import statusNavEvent from "../statusNav/events.js"
-import StatusNav from "../statusNav/StatusNav.js"
+import switchCurrentPage from "../helper.js"
+import { addStatusFile } from "../statusNav/events.js"
 
 const statusTextEntryEvent = () => {
   const statusTextInput = document.querySelector('#statusTextInput')
+  const statusTextContainer = document.querySelector('#statusTextContainer')
+
   statusTextInput.addEventListener('keyup', () => {
     if (statusTextInput.value.trim().length >= 1) {
       document.querySelector('#sendTextButton').style.display = 'block'
@@ -12,13 +14,19 @@ const statusTextEntryEvent = () => {
   })
 
   document.querySelector('#backButton').addEventListener('click', () => {
-    document.querySelector('.current-nav').innerHTML = StatusNav()
-    statusNavEvent()
+    switchCurrentPage('statusPage')
   })
 
   document.querySelector('#colorButton').addEventListener('click', () => {
     const randomColor = Math.floor(Math.random()*16777215).toString(16)
-    document.querySelector('#statusTextContainer').style.backgroundColor = "#" + randomColor
+    statusTextContainer.style.backgroundColor = "#" + randomColor
+  })
+
+  document.querySelector('#sendTextButton').addEventListener('click', () => {
+    console.log('ok');
+    const textValue = statusTextInput.value
+    const entryBackgroundColor = statusTextContainer.style.backgroundColor || '#1C8B82'
+    addStatusFile(textValue, null, entryBackgroundColor)
   })
 }
 
