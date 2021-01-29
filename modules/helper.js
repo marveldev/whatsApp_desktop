af18463d089f0c4081c6a3dd22d43669ee0e8588
Marvel Dev <marvellousalika48@gmail.com>
2021-01-29 06:43:43 +0100

    add statusPage import

--- modules/helper.js
+++ modules/helper.js
@@ -5,6 +5,8 @@ import DefaultNav from "./defaultNav/DefaultNav.js"
 import defaultNavEvents from "./defaultNav/events.js"
 import SettingsNav from "./settingsNav/settingsNav.js"
 import settingsNavEvents from "./settingsNav/events.js"
+import StatusPage from "./statusPage/statusPage.js"
+import StatusNav from "./statusNav/StatusNav.js"
 
 const switchCurrentPage = async (page) => {
   const currentPage = document.querySelector('.current-page')
@@ -23,6 +25,10 @@ const switchCurrentPage = async (page) => {
       currentPage.innerHTML = await ChatPage()
       chatPageEvents()
       chatItemEvents()
+      break;
+    case 'statusPage':
+      currentNav.innerHTML = StatusNav()
+      currentPage.innerHTML = StatusPage()
       break;
     default:
       currentPage.innerHTML = DefaultPage()
