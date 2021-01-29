de25381461685684331e4ccc2d4601384c6f2da2
Marvel Dev <marvellousalika48@gmail.com>
2021-01-29 09:29:34 +0100

    add class names to status-entry-item div contents

--- modules/statusPage/StatusPage.js
+++ modules/statusPage/StatusPage.js
@@ -6,12 +6,10 @@ const StatusPage = () => {
         <p>View your updates</p>
         <div class="status-entry-container">
           <div class="status-entry-item">
-            <button>X</button>
-            <div>
-              <img src="https://images.pexels.com/photos/4119310/pexels-photo-4119310.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500" 
-                class="image" alt="photo">
-              <small>yesterday at 16:14</small>
-            </div>
+            <button class="delete-entry-button">X</button>
+            <img src="https://images.pexels.com/photos/4119310/pexels-photo-4119310.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500" 
+              class="status-entry image" alt="photo">
+            <small>yesterday at 16:14</small>
           </div>
         </div>
       </div>
