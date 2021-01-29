fdef8e6ea83b8d857c55ff3782a091b2d1696dd6
Marvel Dev <marvellousalika48@gmail.com>
2021-01-29 17:15:36 +0100

    add hover effect for status item deleteButton

--- modules/statusPage/events.js
+++ modules/statusPage/events.js
@@ -1,9 +1,22 @@
 import switchCurrentPage from "../helper.js"
 
 const statusPageEvent = () => {
+  const statusEntryItems = document.querySelectorAll('.status-entry-item')
+
   document.querySelector('#closeButton').addEventListener('click', () => {
     switchCurrentPage('defaultPage')
   })
+
+  for (let index = 0; index < statusEntryItems.length; index++) {
+    const statusEntryItem = statusEntryItems[index];
+    statusEntryItem.addEventListener('mouseover', () => {
+      statusEntryItem.firstElementChild.style.visibility = 'visible'
+    })
+
+    statusEntryItem.addEventListener('mouseout', () => {
+      statusEntryItem.firstElementChild.style.visibility = 'hidden'
+    })
+  }
 }
 
 export default statusPageEvent
