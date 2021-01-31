64418e62b9b0cbb136dbe9df3dd8477ea9ca02bf
Marvel Dev <marvellousalika48@gmail.com>
2021-01-31 08:29:05 +0100

    add view-status-page div content

--- modules/viewStatusPage/ViewStatusPage.js
+++ modules/viewStatusPage/ViewStatusPage.js
@@ -1,6 +1,14 @@
 const viewStatusPage = () => {
   return `
-    <h1>hello</h1>
+    <div class="view-status-page">
+      <div id="entryBackground"></div>
+      <button class="previous-button"><i class="material-icons">&#xe5c4;</i></button>
+      <button class="close-button">X</button>
+      <div class="entry-container">
+        <img src="https://images.pexels.com/photos/4119310/pexels-photo-4119310.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"
+          class="entry-photo" alt="photo">
+      </div>
+    </div>
   `
 }
 
