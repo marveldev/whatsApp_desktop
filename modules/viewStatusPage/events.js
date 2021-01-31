a8ac8e067808d5fa512a4d3a6e552e0a7dd3e770
Marvel Dev <marvellousalika48@gmail.com>
2021-01-31 18:05:25 +0100

    add setInterval function to display status-items

--- modules/viewStatusPage/events.js
+++ modules/viewStatusPage/events.js
@@ -1,15 +1,57 @@
 import switchCurrentPage from "../helper.js"
 
 const viewStatusPageEvent = () => {
+  document.querySelector('.entry-container').firstElementChild.classList.add('current')
+  const entryBackground = document.querySelector('#entryBackground')
+  const photoSource = document.querySelector('.current').lastElementChild.src
+  entryBackground.style.backgroundImage = `url(${photoSource})`
+
+  const bars = document.querySelectorAll('.bar')
+  let width = 1
+  let interval = setInterval(() => {
+    if (width >= 100) {
+      const slides = document.querySelectorAll('.status-item')
+      const currentSlide = document.querySelector('.current')
+      currentSlide.classList.remove('current')
+      if (currentSlide.nextElementSibling) {
+        currentSlide.nextElementSibling.classList.add('current')
+        const photoSource = document.querySelector('.current').lastElementChild.src
+        document.querySelector('#entryBackground').style.backgroundImage = `url(${photoSource})`
+        width = 1
+      } else {
+        slides[0].classList.add('current')
+        document.querySelector('.current-nav').style.display = 'block'
+        switchCurrentPage('statusPage')
+        clearInterval(interval)
+      }
+    } else {
+      width++
+      for (let index = 0; index < bars.length; index++) {
+        const bar = bars[index]
+        bar.style.width = width + '%'
+      }
+    }
+  }, 30)
+
   document.querySelector('.previous-button').addEventListener('click', () => {
     document.querySelector('.current-nav').style.display = 'block'
     switchCurrentPage('statusPage')
+    clearInterval(interval)
   })
 
   document.querySelector('.close-button').addEventListener('click', () => {
     document.querySelector('.current-nav').style.display = 'block'
     switchCurrentPage('defaultPage')
+    clearInterval(interval)
   })
+
+  // const statusItems = document.querySelectorAll('.status-item')
+  // for (let index = 0; index < statusItems.length; index++) {
+  //   const statusItem = statusItems[index];
+  //   statusItem.addEventListener('', () => {
+
+  //   })
+  // }
 }
 
 export default viewStatusPageEvent
