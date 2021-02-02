4ec4d31c9ff9559e411d40361039e393d6cafdf1
Marvel Dev <marvellousalika48@gmail.com>
2021-02-02 08:28:27 +0100

    add default-page -photo div container

--- modules/defaultPage/DefaultPage.js
+++ modules/defaultPage/DefaultPage.js
@@ -2,7 +2,7 @@ const DefaultPage = () => {
   return`
     <div class="default-page">
       <div id="defaultPage">
-        <img src="../../assets/default_page_photo.png" alt="phone in hand">
+        <div class="default-page-photo"></div>
         <p>Keep your phone connected</p>
         <span>WhatsApp connects to your phone to sync messages.
           To reduce data usuage, connect your phone to Wi-Fi.
