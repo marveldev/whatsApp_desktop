f202be5467f659c35f2c4b5478eb4d6130eb0bac
Marvel Dev <marvellousalika48@gmail.com>
2021-02-02 08:30:29 +0100

    add theme to local storage

--- modules/settingsNav/events.js
+++ modules/settingsNav/events.js
@@ -1,6 +1,13 @@
 import switchCurrentPage from "../helper.js"
 
 const settingsNavEvents = () => {
+  const theme = localStorage.getItem('theme')
+  const radioButtons = document.querySelectorAll('.radio')
+  const checkedRadioButton = document.querySelector(`input[value=${theme}]`)
+  let checkBox
+
+  theme ? checkedRadioButton.setAttribute('checked', '') : ''
+
   document.querySelector('.back-button').addEventListener('click', () => {
     switchCurrentPage('defaultNav')
   })
@@ -8,6 +15,27 @@ const settingsNavEvents = () => {
   document.querySelector('.theme-button').addEventListener('click', () => {
     document.querySelector('.page-overlay').style.display = 'block'
     document.querySelector('.theme-container').style.display = 'block'
+  })
+
+  document.querySelector('.close-theme-button').addEventListener('click', () => {
+    document.querySelector('.page-overlay').style.display = 'none'
+    document.querySelector('.theme-container').style.display = 'none'
+  })
+
+  for (let index = 0; index <  radioButtons.length; index++) {
+    const  radioButton =  radioButtons[index]
+    radioButton.addEventListener('change', () => {
+      checkBox = radioButton.firstElementChild
+    })
+  }
+
+  document.querySelector('.confirm-theme-button').addEventListener('click', () => {
+    const checkBoxValue = checkBox.value
+    localStorage.setItem('theme', checkBoxValue)
+    document.body.className = checkBoxValue
+    checkBox.setAttribute('checked', '')
+    document.querySelector('.page-overlay').style.display = 'none'
+    document.querySelector('.theme-container').style.display = 'none'
   })
 }
 
