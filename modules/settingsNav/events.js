8b6d602610d32daf6eb6f2bae17ccb88aa3cf159
Marvel Dev <marvellousalika48@gmail.com>
2021-02-02 07:41:31 +0100

    display theme modal on click of theme-button

--- modules/settingsNav/events.js
+++ modules/settingsNav/events.js
@@ -4,6 +4,11 @@ const settingsNavEvents = () => {
   document.querySelector('.back-button').addEventListener('click', () => {
     switchCurrentPage('defaultNav')
   })
+
+  document.querySelector('.theme-button').addEventListener('click', () => {
+    document.querySelector('.page-overlay').style.display = 'block'
+    document.querySelector('.theme-container').style.display = 'block'
+  })
 }
 
 export default settingsNavEvents
