d3f08ce1258040da4d4ba49f5baf04198b6ca05d
Marvel Dev <marvellousalika48@gmail.com>
2021-02-03 10:05:49 +0100

    add topPageOverlay div
    - reposition nav-dropdown-modal container

--- modules/defaultNav/DefaultNav.js
+++ modules/defaultNav/DefaultNav.js
@@ -1,6 +1,7 @@
 const DefaultNav = () => {
   return `
     <div class="default-nav">
+      <div id="topPageOverlay"></div>
       <div class="top-nav">
         <div class="top-nav-buttons">
           <img src="https://images.pexels.com/photos/4119310/pexels-photo-4119310.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500" class="profile-photo" alt="">
@@ -8,18 +9,18 @@ const DefaultNav = () => {
           <button><i class="material-icons">&#xe0b7;</i></button>
           <button class="top-nav-dropdown"><i class="material-icons">&#xe5d4;</i></button>
         </div>
-        <div class="nav-dropdown-modal">
-          <button>New group</button>
-          <button>Create a room</button>
-          <button>Profile</button>
-          <button>Archived</button>
-          <button>Starred</button>
-          <button class="settings-button">Settings</button>
-          <button>Log out</button>
-        </div>
         <div id="searchBoxContainer">
           <textarea class="search-box" placeholder="Search or start a new chat"></textarea>
         </div>
+      </div>
+      <div class="nav-dropdown-modal">
+        <button>New group</button>
+        <button>Create a room</button>
+        <button>Profile</button>
+        <button>Archived</button>
+        <button>Starred</button>
+        <button class="settings-button">Settings</button>
+        <button>Log out</button>
       </div>
       <div class="chat-person-list">
         <button class="chat-person">
