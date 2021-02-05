1b4d069e4857860b5562b838bb0e039552d5a499
Marvel Dev <marvellousalika48@gmail.com>
2021-02-05 09:20:09 +0100

    add pusher to db

--- App.js
+++ App.js
@@ -1,4 +1,4 @@
-import { request } from "./dataStorage.js"
+import { addEntryToDb, request } from "./dataStorage.js"
 import contactPane from "./modules/contactPane/contactPane.js"
 import contactPaneEvents from "./modules/contactPane/events.js"
 import DefaultPage from "./modules/defaultPage/DefaultPage.js"
@@ -21,4 +21,23 @@ request.onsuccess = async () => {
   document.querySelector('.root').innerHTML = App()
   document.body.className = localStorage.getItem('theme')
   contactPaneEvents()
+
+  const pusher = new Pusher('2de3c2f998e996603b10', {
+    cluster: 'eu'
+  })
+
+  const channel = pusher.subscribe('chat')
+  channel.bind('send-message', data => {
+    const itemId = 'id' + Date.parse(new Date()).toString()
+    const chatTime = new Date().toTimeString().substr(0, 5)
+    const { person, message } = data
+
+    const chatObject = {
+      itemId: itemId,
+      person: person,
+      chatTime: chatTime,
+      chatBoxValue: message
+    }
+    addEntryToDb('chatData', chatObject)
+  })
 }
