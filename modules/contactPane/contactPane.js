8ec56d83c274273819e8cdbd0d1d8d884b5064ef
Marvel Dev <marvellousalika48@gmail.com>
2021-02-06 07:59:08 +0100

    change file image.
    - add capitialization to file name
    - change person-info names

--- modules/contactPane/contactPane.js
+++ modules/contactPane/contactPane.js
@@ -1,10 +1,10 @@
-const contactPane = () => {
+const ContactPane = () => {
   return `
     <div class="contact-pane">
       <div id="topPageOverlay"></div>
       <div class="top-nav">
         <div class="top-nav-buttons">
-          <img src="https://images.pexels.com/photos/4119310/pexels-photo-4119310.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500" class="profile-photo" alt="">
+          <img src="https://images.pexels.com/photos/6306487/pexels-photo-6306487.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500" class="profile-photo" alt="photo">
           <button class="status-button"><i class="material-icons">&#xe1af;</i></button>
           <button><i class="material-icons">&#xe0b7;</i></button>
           <button class="top-nav-dropdown"><i class="material-icons">&#xe5d4;</i></button>
@@ -25,10 +25,70 @@ const contactPane = () => {
       </div>
       <div class="chat-person-list">
         <button class="chat-person">
-          <img src="https://images.pexels.com/photos/4119310/pexels-photo-4119310.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500" 
+          <img src="https://images.pexels.com/photos/6306487/pexels-photo-6306487.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500" 
             class="image" alt="photo">
           <div class="more-info">
-            <strong>Jack Williams</strong>
+            <p class="name">Liam Benjamin</p>
+            <div>
+              <p>Hello</p>
+            </div>
+          </div>
+        </button>
+        <button class="chat-person">
+          <img src="https://images.pexels.com/photos/2880122/pexels-photo-2880122.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500" 
+            class="image" alt="photo">
+          <div class="more-info">
+            <strong>Jude Ugo</strong>
+            <div>
+              <p>Hello</p>
+            </div>
+          </div>
+        </button>
+        <button class="chat-person">
+          <img src="https://images.pexels.com/photos/6706036/pexels-photo-6706036.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500" 
+            class="image" alt="photo">
+          <div class="more-info">
+            <strong>Testimony Gift</strong>
+            <div>
+              <p>Hello</p>
+            </div>
+          </div>
+        </button>
+        <button class="chat-person">
+          <img src="https://images.pexels.com/photos/6124401/pexels-photo-6124401.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500" 
+            class="image" alt="photo">
+          <div class="more-info">
+            <p class="name">Jane White</p>
+            <div>
+              <p>Hello</p>
+            </div>
+          </div>
+        </button>
+        <button class="chat-person">
+          <img src="https://images.pexels.com/photos/6677447/pexels-photo-6677447.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500" 
+            class="image" alt="photo">
+          <div class="more-info">
+            <strong>Ferenda Little</strong>
+            <div>
+              <p>Hello</p>
+            </div>
+          </div>
+        </button>
+        <button class="chat-person">
+          <img src="https://images.pexels.com/photos/6550709/pexels-photo-6550709.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500" 
+            class="image" alt="photo">
+          <div class="more-info">
+            <p class="name">Charlotte Emma</p>
+            <div>
+              <p>Hello</p>
+            </div>
+          </div>
+        </button>
+        <button class="chat-person">
+          <img src="https://images.pexels.com/photos/6561125/pexels-photo-6561125.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500" 
+            class="image" alt="photo">
+          <div class="more-info">
+            <strong>Adanna Fiona</strong>
             <div>
               <p>Hello</p>
             </div>
@@ -38,67 +98,7 @@ const contactPane = () => {
           <img src="https://images.pexels.com/photos/4119310/pexels-photo-4119310.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500" 
             class="image" alt="photo">
           <div class="more-info">
-            <strong>Jack Williams</strong>
-            <div>
-              <p>Hello</p>
-            </div>
-          </div>
-        </button>
-        <button class="chat-person">
-          <img src="https://images.pexels.com/photos/4119310/pexels-photo-4119310.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500" 
-            class="image" alt="photo">
-          <div class="more-info">
-            <strong>Jack Williams</strong>
-            <div>
-              <p>Hello</p>
-            </div>
-          </div>
-        </button>
-        <button class="chat-person">
-          <img src="https://images.pexels.com/photos/4119310/pexels-photo-4119310.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500" 
-            class="image" alt="photo">
-          <div class="more-info">
-            <strong>Jack Williams</strong>
-            <div>
-              <p>Hello</p>
-            </div>
-          </div>
-        </button>
-        <button class="chat-person">
-          <img src="https://images.pexels.com/photos/4119310/pexels-photo-4119310.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500" 
-            class="image" alt="photo">
-          <div class="more-info">
-            <strong>Jack Williams</strong>
-            <div>
-              <p>Hello</p>
-            </div>
-          </div>
-        </button>
-        <button class="chat-person">
-          <img src="https://images.pexels.com/photos/4119310/pexels-photo-4119310.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500" 
-            class="image" alt="photo">
-          <div class="more-info">
-            <strong>Jack Williams</strong>
-            <div>
-              <p>Hello</p>
-            </div>
-          </div>
-        </button>
-        <button class="chat-person">
-          <img src="https://images.pexels.com/photos/4119310/pexels-photo-4119310.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500" 
-            class="image" alt="photo">
-          <div class="more-info">
-            <strong>Jack Williams</strong>
-            <div>
-              <p>Hello</p>
-            </div>
-          </div>
-        </button>
-        <button class="chat-person">
-          <img src="https://images.pexels.com/photos/4119310/pexels-photo-4119310.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500" 
-            class="image" alt="photo">
-          <div class="more-info">
-            <strong>Jack Williams</strong>
+            <p class="name">Jack Williams</p>
             <div>
               <p>Hello</p>
             </div>
@@ -129,4 +129,4 @@ const contactPane = () => {
   `
 }
 
-export default contactPane
+export default ContactPane
