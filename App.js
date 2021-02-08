20ff4f66d62f8c59fab60821bfcb8c31195133af
Marvel Dev <marvellousalika48@gmail.com>
2021-02-08 08:32:35 +0100

    import pusher events

--- App.js
+++ App.js
@@ -1,8 +1,8 @@
-import { addEntryToDb, request } from "./dataStorage.js"
-import chatPageEvents, { addChatItemToDom } from "./modules/chatPage/events.js"
+import { request } from "./dataStorage.js"
 import ContactPane from "./modules/contactPane/ContactPane.js"
-import contactPaneEvents from "./modules/contactPane/events.js"
+import addContactPaneEvents from "./modules/contactPane/events.js"
 import DefaultPage from "./modules/defaultPage/DefaultPage.js"
+import { addChatPusherEvents, addStatusPusherEvents } from "./pusher.js"
 
 const App = () => {
   return `
@@ -21,29 +21,7 @@ const App = () => {
 request.onsuccess = async () => {
   document.querySelector('.root').innerHTML = App()
   document.body.className = localStorage.getItem('theme')
-  contactPaneEvents()
-
-  const pusher = new Pusher('2de3c2f998e996603b10', {
-    cluster: 'eu'
-  })
-
-  const channel = pusher.subscribe('chat')
-  channel.bind('send-message', data => {
-    const { person, message } = data
-    const itemId = 'id' + Date.parse(new Date()).toString()
-    const chatTime = new Date().toTimeString().substr(0, 5)
-    const chatObject = {
-      itemId: itemId,
-      person: person,
-      chatTime: chatTime,
-      chatBoxValue: message
-    }
-
-    const chatPageDiv = document.querySelector('.chat-page')
-    if (chatPageDiv) {
-      addChatItemToDom(person, message)
-    } else {
-      addEntryToDb('chatData', chatObject)
-    }
-  })
+  addContactPaneEvents()
+  addChatPusherEvents()
+  addStatusPusherEvents()
 }
